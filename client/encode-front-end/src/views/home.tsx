76ada6c8731479bd88fdc7c5fd24acd960ecc6f4
artificialvirus <olperander@outlook.com>
2024-03-08 17:40:48 +0000

    Initial commit.

---
+++ client/encode-front-end/src/views/home.tsx
@@ -0,0 +1,21 @@
+// File: client/encode-front-end/src/views/home.tsx
+// This file contains home view
+// Importing necessary modules
+import React from 'react';
+// import css
+
+
+// Defining Home component 
+const Home: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Welcome to Encode Frontend</h1>
+      <p>
+        Edit <code>src/views/home.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+};
+
+export default Home;
