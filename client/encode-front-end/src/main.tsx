bdbae4dacebdcdca3d7253f016a680d3e14c2be9
ashprit <ashpritmehra@gmail.com>
2024-03-09 13:12:35 +0000

    Landing Page is Finished.

--- client/encode-front-end/src/main.tsx
+++ client/encode-front-end/src/main.tsx
@@ -4,11 +4,19 @@ import './index.css'
 // import Dashboard from './views/dashboard.tsx'
 import Navbar from './components/navbar.tsx'
 import About from './components/about.tsx'
+import Landing from './components/landing.tsx'
+import Landingend from './components/landingend.tsx'
+import Contacts from './components/contacts.tsx'
+import Footer from './components/footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar />
+    <Landing/>
+    <Landingend/>
     <About/>
+    <Contacts/>
+    <Footer/>
 
   </React.StrictMode>,
 )
