e1c44087ad8596f1755900efcfc5021558e8fea8
artificialvirus <olperander@outlook.com>
2024-03-10 07:57:53 +0000

    .

--- client/encode-front-end/src/main.tsx
+++ client/encode-front-end/src/main.tsx
@@ -8,13 +8,13 @@ import './index.css'
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 import Payments from './views/Payments';
 import Dashboard from './views/dashboard.tsx'
-import Home from './views/home.tsx'
+import HomePage from './views/home.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/payments" element={<Payments />} />
         <Route path="/dashboard" element={
           <ProtectedRoute>
